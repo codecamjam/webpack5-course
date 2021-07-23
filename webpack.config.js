0ec0e5bfb063158db24882da8aac92d7adc8a13b
codecamjam <camerontaghlabi@gmail.com>
2021-07-22 22:54:09 -0400

    l10 handle images with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,21 @@
-const path = require('path')
+const path = require("path");
 module.exports = {
   entry: "./src/index.js",
   output: {
     filename: "bundle.js",
-    path: path.resolve(__dirname, './dist'),
+    path: path.resolve(__dirname, "./dist"),
   },
   mode: "none",
+  module: {
+    rules: [
+      // {
+      //   test: /\.(ttf)$/,
+      //   type: "asset/resource",
+      // },
+      {
+        test: /\.(png|jpg)$/,
+        type: "asset/resource",
+      },
+    ],
+  },
 };
