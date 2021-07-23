81c88e09a03efb3fac256eaab76e832b085ad8a5
codecamjam <camerontaghlabi@gmail.com>
2021-07-23 11:35:34 -0400

    l18 using latest js features with babel;

--- src/components/hello-world-button/hello-world-button.js
+++ src/components/hello-world-button/hello-world-button.js
@@ -1,10 +1,19 @@
 import "./hello-world-button.scss";
 
 class HelloWorldButton {
+  //this is example of class property and
+  //not supported by major browsers
+  //most only allow methods inside js classes but not
+  //properties
+  //actually this is supported out of the box now
+  //but any brand new features would probably throw an error
+  //like pipeline operator
+  buttonCssClass = "hello-world-button";
+
   render() {
     const button = document.createElement("button");
     button.innerHTML = "Hello world";
-    button.classList.add("hello-world-button");
+    button.classList.add(this.buttonCssClass);
     const body = document.querySelector("body");
     button.onclick = function () {
       const p = document.createElement("p");
--- webpack.config.js
+++ webpack.config.js
@@ -29,16 +29,33 @@ module.exports = {
       },
       {
         test: /\.scss$/,
-        /**
-         * WEBPACK PROCESS LOADERS FROM RIGHT TO LEFT!!!!
-         * 1 invoke sass loader which converts sass to css
-         * 2 invoke css loader which converts css to js representation
-         * 3 invoke style loader which creates style tags inside html tags
-         * and injects the css into it
-         * DONT FORGET TO:
-         * npm i sass-loader sass --save-dev
-         */
         use: ["style-loader", "css-loader", "sass-loader"],
+      },
+
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            /**
+             * env preset compiles ecma 6,7,8,etc down to ecma 5
+             * so it supports latest ecma standard
+             *
+             * during tthe time of this tutorial series release,
+             * class properties werent part of the official ecma specs
+             * so we also needed the plugin below
+             * AND if you wanted to use another modern js feature
+             * not supported by major browsers yet,
+             * you should find a babel plugin for that and include
+             * in the list of plugins
+             *
+             * npm i @babel/core babel-loader @babel/preset-env @babel/plugin-proposal-class-properties --save-dev
+             */
+            presets: ["@babel/env"],
+            plugins: ["@babel/plugin-proposal-class-properties"],
+          },
+        },
       },
     ],
   },
