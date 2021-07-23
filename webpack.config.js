c40c3e61149023936125b757d7464b4e6c188b4d
codecamjam <camerontaghlabi@gmail.com>
2021-07-23 12:50:52 -0400

    l25 generating html files automatically during webpack build process

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,22 @@ const path = require("path");
 const TerserPlugin = require("terser-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
   output: {
     filename: "bundle.[contenthash].js",
     path: path.resolve(__dirname, "./dist"),
-    publicPath: "dist/",
+    /**
+     * because we had publicPath set to -> dist/,
+     * when we used html-webpack-plugin for first time,
+     * our refs were wrong
+     * dist/bundle.f4a31e2d7bee575e203c.js
+     *
+     * so thats why we set public path to empty now
+     */
+    publicPath: "",
   },
   mode: "none",
   module: {
@@ -52,41 +61,7 @@ module.exports = {
     new MiniCssExtractPlugin({
       filename: "styles.[contenthash].css",
     }),
-    /**
-     * this auto cleans our dist folder and removes clutter
-     * when we have multiple hashed files from builds
-     */
-    // new CleanWebpackPlugin(),
-    //we can also add options: in this example, we have another folder (build)
-    new CleanWebpackPlugin({
-      /**
-       * this removes old files before webpack generates new files
-       * and you can specify an array of file patterns which you want to remove
-       * and these patterns are relative to output in
-       *  path.resolve(__dirname, "./dist")
-       */
-
-      cleanOnceBeforeBuildPatterns: [
-        /**
-         * THIS MEANS TO REMOVE ALL THE FILES TOGETHER
-         * WITH SUBDIRECTORIES INSIDE DIST FOLDER
-         * NO MATTER HOW NESTED LEVELS
-         *
-         * AND THIS IS HOW CLEAN ONCE BEFORE BUILT PATTERNS
-         * WORKS BY DEFAULT IF YOU DONT MODIFY IT
-         *
-         *
-         */
-        "**/*",
-        /**
-         * However, if you want to remove the files
-         * outside of the dist folder,
-         * you should specify an absolute path to file patterns
-         * this will remove all the files together with sub
-         * folders inside the build folder
-         */
-        path.join(process.cwd(), "build/**/*"),
-      ],
-    }),
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin(),
   ],
 };
