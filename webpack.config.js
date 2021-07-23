6115d61257d5980cbcd5303768547a681273e422
codecamjam <camerontaghlabi@gmail.com>
2021-07-23 10:13:42 -0400

    l12 asset/inline module type

--- webpack.config.js
+++ webpack.config.js
@@ -4,18 +4,15 @@ module.exports = {
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "./dist"),
-    //publicPath: 'auto' in webpack 5 this is set automatically
-    //in webpack 4, the default was ''
     publicPath: "dist/",
-    //for a cdn
-    // publicPath: "http://some-cdn.com/",
   },
   mode: "none",
   module: {
     rules: [
       {
         test: /\.(png|jpg)$/,
-        type: "asset/resource",
+        // type: "asset/resource", //good for massive files
+        type: "asset/inline", //good for svgs and small imgs
       },
     ],
   },
